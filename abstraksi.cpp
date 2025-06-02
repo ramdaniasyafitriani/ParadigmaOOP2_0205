feb6ea7f1a3b75c4605a33a500113595b8bed3e4
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-02 18:23:09 +0700

    membuat method untuk mengisi nilai

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -7,5 +7,12 @@ private :
     string x, y;
 
 public :
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
 };
 
